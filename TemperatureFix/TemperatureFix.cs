b66fc23eaa7d0455dba552e108ff136d3a365f10
Bao Tran <bao.tran1994@yahoo.com>
2019-10-16 22:25:44 -0700

    Add project files.

---
+++ TemperatureFix/TemperatureFix.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Harmony;
+using UnityEngine;
+
+namespace TemperatureFix
+{
+    [HarmonyPatch(typeof(Substance), "SpawnResource")]
+    internal class SpawnResource_TempFix
+    {
+        private static void Postfix(ref GameObject __result)
+        {
+            PrimaryElement primeElement = __result.GetComponent<PrimaryElement>();
+            primeElement.SetMassTemperature(primeElement.Mass, primeElement.InternalTemperature);
+        }
+    }
+}
